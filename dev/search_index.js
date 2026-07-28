f24ce39ea30097ff3d800e54aa2c753efeed4697
Documenter.jl <documenter@juliadocs.github.io>
2026-07-28 15:35:12 +0000

    build based on 4cd6740

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#LagrangeFourSquares","page":"Home","text":"Documentation for LagrangeFourSquares.\n\n","title":"LagrangeFourSquares"}]
+}
